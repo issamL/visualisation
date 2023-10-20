08b76bf09443069da49d443ff0ff150258c33b55
Issam Lhous <lhous.issam99@gmail.com>
2023-10-20 13:28:32 +0200

    initial commit

---
+++ src/main/java/com/api/visualisation/VisualisationApplication.java
@@ -0,0 +1,13 @@
+package com.api.visualisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualisationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/visualisation/VisualisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.visualisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
